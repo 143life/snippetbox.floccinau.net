32f3bc236333a5436eb04b7f37b9f99cc1426056
Floccinau <argonavt28@mail.ru>
2025-08-15 20:11:49 +0300

    Methods to work with Request and Response in handlers

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Define a home handler function which writes a byte slice containing
@@ -17,11 +19,68 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 // snippetView handler function
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	// Extract the value of the id parameter from the query string and try to
+	// convert it to an integer using the strconv.Atoi() function. If it can't
+	// be converted to an integer, or the value is less than 1, we return a 404 page
+	// not found response
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	// Use the fmt.Fprintf() function to interpolate the id value with our response
+	// and write it to the http.ResponseWriter
+	fmt.Fprintf(w, "Display a specific snippet with ID %d", id)
+
+	w.Write([]byte("\nDisplay a specific snippet..."))
 }
 
 // snippetCreate handler function
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// Use r.Method to check whether the request is using POST or not.
+	if r.Method != "POST" {
+		// Use the Header().Set() method to add an 'Allow: POST' header to the
+		// response header map. The first parameter is the header name, and
+		// the second parameter is the header value.
+		// call this method BEFORE calling WriteHeader() and Write()
+		w.Header().Set("Allow", http.MethodPost)
+
+		// Set a new cache-control neader. If an existing "Cache-Control" header exists
+		// it will be overwritten
+		w.Header().Set("Cache-Control", "public, max-age=31536000")
+		// In contrast, the Add() method appends a new "Cache-Control" header and can
+		// be called multiple times.
+		w.Header().Add("Cache-Control", "public")
+		w.Header().Add("Cache-Control", "max-age=31536000")
+		// Delete all values for the "Cache-Control" header.
+		w.Header().Del("Cache-Control")
+		//Retrieve the first value for the "Cache-Control" header.
+		w.Header().Get("Cache-Control")
+		// Retrieve a slice of all values for the "Cache-Control" header.
+		w.Header().Values("Cache-Control")
+		// If need to avoid textproto.CanonicalMIMEHeaderKey()
+		w.Header()["X-XSS-Protection"] = []string{"1; mode=block"}
+		// Remove system generated headers
+		w.Header()["Date"] = nil
+
+		// // if it's not, use the w.WriteHeader() method to send a 405 status
+		// // code and the w.Write() method to write a "Method Not Allowed"
+		// // response body. We then return from the function so that the
+		// // subsequent code is not executed
+		// //w.WriteHeader(405)
+		// //w.Write([]byte("Method Not Allowed"))
+
+		// // //w.Header().Set("Content-Type", "application/json")
+		// // //w.Write([]byte(`{"name":"Alex"}`))
+
+		// Use the http.Error() function to send a 405 status code and "Method Not
+		// Allowed" string as the response body.
+		// There we're now passing our http.ResponseWriter to another function,
+		// which sends a response to the user of us
+		http.Error(w, "MethodNotAllowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
 
