ed9444930ffe08a031dc181dce4aea35f2fb8db9
Floccinau <argonavt28@mail.ru>
2025-09-23 12:39:31 +0300

    Chapter 4.8: Multiple-record SQL queries

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,8 @@ package main
 import (
 	"errors"
 	"fmt"
-	"html/template"
+
+	//"html/template"
 	"net/http"
 	"strconv"
 
@@ -19,37 +20,48 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Chapter 4.8: Multiple-record SQL queries |
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	// Chapter 4.8: Multiple-record SQL queries
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
+	}
+
 	// Initialize a slice containing the paths to the two files. It's important
 	// to note that the file containing our base template must be the *first*
 	// file in the slice.
-	files := []string{
-		"./ui/html/base.tmpl.html",
-		"./ui/html/partials/nav.tmpl.html",
-		"./ui/html/pages/home.tmpl.html",
-	}
+	//files := []string{
+	//	"./ui/html/base.tmpl.html",
+	//	"./ui/html/partials/nav.tmpl.html",
+	//	"./ui/html/pages/home.tmpl.html",
+	//}
 
 	// Use the template.ParseFiles() function to read the files and store the
 	// templates in a template set. Notice that we can pass the slice of file
 	// paths as a variadic parameter?
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		// Chapter 3.3: Dependency injection |
-		// Because the home handler function is now a method against application
-		// it can access its fields, including the error logger. We'll write the log
-		// message to this instead of the standart logger.
-		// app.errorLog.Println(err.Error())
-		// Chapter 3.4: Cenralized error handling |
-		// Use the serverError() helper
-		app.serverError(w, err)
-		return
-	}
+	//ts, err := template.ParseFiles(files...)
+	//if err != nil {
+	// Chapter 3.3: Dependency injection |
+	// Because the home handler function is now a method against application
+	// it can access its fields, including the error logger. We'll write the log
+	// message to this instead of the standart logger.
+	// app.errorLog.Println(err.Error())
+	// Chapter 3.4: Cenralized error handling |
+	// Use the serverError() helper
+	//	app.serverError(w, err)
+	//	return
+	//}
 
 	// Use the ExecuteTemplate() method to write the content of the "base"
 	// template as the response body.
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		app.serverError(w, err)
-	}
+	//err = ts.ExecuteTemplate(w, "base", nil)
+	//if err != nil {
+	//	app.serverError(w, err)
+	//}
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -106,5 +106,70 @@ func (m *SnippetModel) Get(id int) (*Snippet, error) {
 // Chapter 4.5: Designing a database model |
 // This will return the 10 most recently created snippets.
 func (m *SnippetModel) Latest() ([]*Snippet, error) {
-	return nil, nil
+	// Chapter 4.8: Multiple-record SQL queries |
+	//  Write the SQL statement we want to execute
+	stmt := `SELECT id, title, content, created, expires
+	FROM snippets
+	WHERE expires > NOW()
+	ORDER BY id DESC LIMIT 10`
+
+	// Chapter 4.8: Multiple-record SQL queries |
+	// Use the Query() method on the connection pool to execute our
+	// SQL statement. This returns a sql.Rows resultset containing the result of
+	// our query.
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+
+	// Chapter 4.8: Multiple-record SQL queries |
+	// We defer rows.Close() to ensure the SQL.rows resultset is
+	// always properly closed before the Latest() method returns. This defer
+	// statement should come *after* you check for an error from the Query()
+	// method. Otherwise, if Query() returns an error, you'll get a panic
+	// trying to close a nil resultset.
+	defer rows.Close()
+
+	// Chapter 4.8: Multiple-record SQL queries |
+	// Initialize an empty slice to hold the Snippet structs.
+	snippets := []*Snippet{}
+
+	// Chapter 4.8: Multiple-record SQL queries |
+	// Use rows.Next to iterate through the rows in the resultset. This
+	// prepares the first (and then each subsequent) row to be acted on by the
+	// rows.Scan() method. If iteration over all the rows completes then the
+	// resultset automatically closes itself and frees-up the underlying
+	// database connection.
+	for rows.Next() {
+		// Chapter 4.8: Multiple-record SQL queries |
+		// Create a pointer to a new zeroed Snippet struct.
+		s := &Snippet{}
+		// Chapter 4.8: Multiple-record SQL queries |
+		// Use rows.Scan() to copy the values from each field in the row to the
+		// new Snippet object that we created. Again, the arguments to row.Scan()
+		// must be pointers to the place you want to copy the data into, and the
+		// number of arguments must be exactly the same as the number of
+		// columns returned by your statement.
+		err = rows.Scan(&s.ID, &s.title, &s.content, &s.created, &s.expires)
+		if err != nil {
+			return nil, err
+		}
+
+		// Chapter 4.8: Multiple-record SQL queries |
+		// Append it to the slice of snippets
+		snippets = append(snippets, s)
+	}
+
+	// Chapter 4.8: Multiple-record SQL queries |
+	// When the rows.Next() loop has finished we call rows.Err() to retrieve any
+	// error thet was encountered during the iteration. it's important to
+	// call this - don't assume that a successful iteration was completed
+	// over the whole resultset.
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	// Chapter 4.8: Multiple-record SQL queries
+	// If everything went OK then return the Snippets slice.
+	return snippets, nil
 }
